1892d8bf6e321ed2cab6af47222d39eb88a72462
yves-ganza <yveslebon@proton.me>
2024-05-01 15:38:40 -0400

    Create Models

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 def index(request):
-    return render(request, 'core/index.html')
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
