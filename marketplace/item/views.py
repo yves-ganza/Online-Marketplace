e6876e4bcf3e4b0f3feef0cc96794e8e7b37940b
yves-ganza <yveslebon@proton.me>
2024-05-15 14:06:59 -0400

    Implement add-item functionality

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Item
+from .forms import NewItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -9,3 +11,22 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items,
     })
+
+
+@login_required
+def new(request):
+    if(request.method == 'POST'):
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+        return render(request, 'item/form.html', {
+            'form': form,
+            'title': 'New Item'
+        })
