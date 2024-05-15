63e32044d8fd9b574d08af6dd568488456adcbe8
yves-ganza <yveslebon@proton.me>
2024-05-15 15:28:13 -0400

    Create user dashboard

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -24,5 +24,6 @@ from core.views import index, contact
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
